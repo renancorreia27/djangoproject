3abf034ce7ec41412f2b06039d4251df582616d8
Renan Machado <renanmachadocorreia27@gmail.com>
2025-10-30 09:07:24 -0300

    Atualização do product_create.html

--- products/forms.py
+++ products/forms.py
@@ -6,7 +6,17 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'price', 'description']
         widgets = {
-            'name': forms.TextInput(attrs={
-                
-            })
+            'name': forms.TextInput(attrs={ # attrs = atributos html
+                'class': 'form-control', # como o css vai reconhecer o campo 'name'
+                'placeholder': 'Nome do produto' # texto que aparece no campo
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Preço em reais'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Descrição do produto',
+                'rows': 3
+            }),
         }
